3cde1d10f00d319e4346cb359c7ad695b07e0809
pchai <prempreychhay@gmail.com>
2024-08-11 20:53:18 -0700

    Update by using namepsace std; to minimize using 'std::'

--- MyFirstCPPApp.cpp
+++ MyFirstCPPApp.cpp
@@ -1,11 +1,12 @@
 #include <iostream> // Header file, a library
+using namespace std;    // Help by minimize using std::
 
 int main()  // Main function
 {
     // std is the namepace for cout
-    std::cout << "Hello World!\n";  // This will output to the console Hello World!
-    std::cout << "My name is Prem." << std::endl;
-    std::cout << "Today is Sunday.";
+    cout << "Hello World!\n";  // This will output to the console Hello World!
+    cout << "My name is Prem." << endl;
+    cout << "Today is Sunday.";
 
     // Use to get rid of extra info on the console
     system("pause>0");
