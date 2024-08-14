ada28cd069e45df7218b8756e1f6460cbe82f957
pchai <prempreychhay@gmail.com>
2024-08-14 09:55:05 -0700

    Ciphering words using ASCII

--- MyFirstCPPApp.cpp
+++ MyFirstCPPApp.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main()
 {
-    // ASCII -- American Standard Code for Information Interchange
-    cout << (int)'a' << endl;   // Casting operator
-    cout << int('a') << endl;   // Output 97
-    cout << int('A') << endl;   // Output 65
-    cout << char(65) << endl;   // Output A
+    // Ciphering words using ASCII
+    char c1, c2, c3, c4, c5;    // You can assign variables like this since they are of the same type
+    cout << "Enter 5 letters: ";
+    cin >> c1 >> c2 >> c3 >> c4 >> c5;  // Getting input; will not take space and enter as character
+    cout << "\nASCII message: " << int(c1) << " " << int(c2) << " " << int(c3)
+        << " " << int(c4) << " " << int(c5);
 
     system("pause>0");
 }
