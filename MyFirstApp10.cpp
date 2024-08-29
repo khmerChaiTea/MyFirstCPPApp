a8b217fcebab920ef66d4c68da3ec3efc861a61d
pchai <prempreychhay@gmail.com>
2024-08-29 09:08:41 -0700

    Operators in C++ (arithmetic, relational, logical, assignment operators)

---
+++ MyFirstApp10.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//// Operators
+	//// +, -, *, /, %
+
+	//cout << 5 + 2 << "\n";
+	//cout << 5 / 2 << "\n"; // Output 2 - drop the remainder
+	//cout << 5 % 2 << "\n\n"; // Output 1 - Remainder
+
+	//// ++, --
+	//int counter = 7;
+	//counter++;
+	//cout << counter << "\n";
+	//counter--;
+	//cout << counter << "\n\n";
+
+	//int counter2 = 7;
+	//cout << ++counter2 << "\n";
+	//cout << counter2-- << "\n";
+	//cout << counter2 << "\n\n";
+
+	////system("cls"); // Cleared the console
+
+	//// <, >, <=, >=, ==, !=
+	//int a = 5, b = 8;
+	//cout << (a < b); // 1 is true 0 is false
+	//cout << (a > b); // 0
+	//cout << (a >= b); // 0
+	//cout << (a <= b); // 1
+	//cout << (a == b); // 0
+	//cout << (a != b); // 1
+
+	//// &&, ||, !
+	//cout << (a == 5 && b == 5); // 0
+	//cout << (a == 5 || b == 5); // 1
+	//cout << !(a == 5 || b == 5); // 0 (not operator)
+	//cout << (a == 5 && b == 5 + 3); // 1 (true for a=5 and b = 8)
+
+	//// =. +=, -=, *=, /=, %=
+	//int x = 5;
+	//x += 7; // x = x + 7
+	//cout << x << "\n";
+
+	// What is going to be the result of tje following lines?
+
+	int a = 5, b = 4;
+	cout << a + (b++) << "\n"; // 9
+	cout << (--a) + b << "\n\n"; // 9
+
+	cout << !(true && false) << "\n"; // 1
+	cout << (true && false || true) << "\n\n"; //1
+
+	int x = 7;
+	cout << (++x <= 9 && x + 2 >= 10) << "\n\n"; // 1
+
+	int y = 3;
+	cout << (y == y++) << "\n\n"; // 1
+
+	float z = 8;
+	z += 2;
+	cout << z << "\n"; // 10
+	z /= 2;
+	cout << z << "\n\n"; // 5
+
+	system("pause>0");
+}
--- MyFirstCPPApp.cpp
+++ MyFirstCPPApp.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    // If Else Statement (Nested)
-    // Tasks:
-    // Users enters side lengths of a triangle (a, b, c)
-    // Program should write out whether that triangle is equilateral, isosceles or scalene
-    float a, b, c;
-    cout << "a, b, c: ";
-    cin >> a >> b >> c;
-
-    if (a == b && b == c)   // Please check
-        cout << "Equilateral triangle"; // If only pnly one code you can omit '{}'
-    else
-    {
-        if (a != b && a != c && b != c)
-            cout << "Scalene triangle";
-        else
-            cout << "Isosceles triangle";
-    }
-
-    system("pause>0");
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    // If Else Statement (Nested)
+//    // Tasks:
+//    // Users enters side lengths of a triangle (a, b, c)
+//    // Program should write out whether that triangle is equilateral, isosceles or scalene
+//    float a, b, c;
+//    cout << "a, b, c: ";
+//    cin >> a >> b >> c;
+//
+//    if (a == b && b == c)   // Please check
+//        cout << "Equilateral triangle"; // If only pnly one code you can omit '{}'
+//    else
+//    {
+//        if (a != b && a != c && b != c)
+//            cout << "Scalene triangle";
+//        else
+//            cout << "Isosceles triangle";
+//    }
+//
+//    system("pause>0");
+//}
 
