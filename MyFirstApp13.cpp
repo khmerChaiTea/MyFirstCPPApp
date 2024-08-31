94f7e57b7be9226312acd045dce0cca480665a5d
pchai <prempreychhay@gmail.com>
2024-08-31 10:15:10 -0700

    Switch/case statement part 1 (Build Calculator app)

--- MyFirstApp13.cpp
+++ MyFirstApp13.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	int hostUserNum, guestUserNum;
-	cout << "Host: ";
-	cin >> hostUserNum;
-	system("cls"); // Cleared console
-	cout << "Guest: ";
-	cin >> guestUserNum;
-
-	// Ternary operator
-	(hostUserNum == guestUserNum)? cout << "Correct!\n" : cout << "Failed!\n";
-
-	//if (hostUserNum == guestUserNum)
-	//	cout << "Correct!\n";
-	//else
-	//	cout << "Failed!\n";
-
-	system("pause>0");
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int hostUserNum, guestUserNum;
+//	cout << "Host: ";
+//	cin >> hostUserNum;
+//	system("cls"); // Cleared console
+//	cout << "Guest: ";
+//	cin >> guestUserNum;
+//
+//	// Ternary operator
+//	(hostUserNum == guestUserNum)? cout << "Correct!\n" : cout << "Failed!\n";
+//
+//	//if (hostUserNum == guestUserNum)
+//	//	cout << "Correct!\n";
+//	//else
+//	//	cout << "Failed!\n";
+//
+//	system("pause>0");
+//}
