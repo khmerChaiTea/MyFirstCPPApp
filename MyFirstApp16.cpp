c06acbf007269555be8f629eb7029b5f291f6fbe
pchai <prempreychhay@gmail.com>
2024-09-01 07:57:20 -0700

    While loop part 1 + infinite loop example

---
+++ MyFirstApp16.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// Write out al the numbers between 100-500 that are
+	// divisible by 3 and 5
+
+	int counter = 100;
+
+	while (counter <= 500)
+	{
+		if (counter % 3 == 0 && counter % 5 == 0)
+			cout << counter << " is divisible by 3 and 5\n";
+
+		counter++;
+	}
+
+	system("pause>0");
+}
